81f3ecfdc7a9d06d9a3fc8f141593aabde59d863
A7Aima <aimaabhilash@gmail.com>
2020-11-06 08:31:50 -0800

    Initial commit climate change app

---
+++ android/app/src/main/kotlin/example/climate_change/MainActivity.kt
@@ -0,0 +1,6 @@
+package example.climate_change
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/find';
+
+const apiKey = 'd5bcb8b86658d6201e4c04ae4062d7f1';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 30.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 15.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: "Enter City Name",
+  hintStyle: TextStyle(color: Colors.grey),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
